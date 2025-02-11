5c7b33406fd090c6b65f666d51946a9070a648e0
ddheer29 <divyangdheer11@gmail.com>
2025-02-11 23:52:31 +0530

    pick reel screen

--- src/screens/reel/PickReelScreen.tsx
+++ src/screens/reel/PickReelScreen.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import CustomView from '../../components/global/CustomView'
+import CustomHeader from '../../components/global/CustomHeader'
 
 const PickReelScreen = () => {
   return (
-    <View>
-      <Text>PickReelScreen</Text>
-    </View>
+    <CustomView>
+      <SafeAreaView style={styles.margin}>
+        <CustomHeader title='New Reel' />
+      </SafeAreaView>
+      <View>
+
+      </View>
+    </CustomView>
   )
 }
 
 export default PickReelScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  margin: {
+    margin: 10,
+  }
+})
