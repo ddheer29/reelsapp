47be3a77d896388087d90c10b4b1c9f9e65168ca
ddheer29 <divyangdheer11@gmail.com>
2025-02-19 23:16:54 +0530

    reel item card for home screen

---
+++ src/components/feed/ReelItemCard.tsx
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native'
+import React, { FC } from 'react'
+import { screenHeight, screenWidth } from '../../utils/Scaling';
+import { Colors } from '../../constants/Colors';
+import FastImage from 'react-native-fast-image';
+import ReelCardLoader from '../loader/ReelCardLoader';
+
+interface ReelItemCardProps {
+  style?: ViewStyle;
+  loading: boolean;
+  item: any;
+  onPressReel: () => void;
+}
+
+const ReelItemCard: FC<ReelItemCardProps> = ({
+  style,
+  loading,
+  item,
+  onPressReel,
+}) => {
+  return (
+    <View style={[styles.card, style]}>
+      {
+        loading ? (
+          <ReelCardLoader style={styles.skeltonLoader} />
+        ) : (
+          <TouchableOpacity onPress={onPressReel}>
+            <FastImage
+              source={{ uri: item.thumbUri, priority: FastImage.priority.high }}
+              style={styles.img}
+              resizeMode='cover'
+            />
+          </TouchableOpacity>
+        )
+      }
+    </View>
+  )
+}
+
+export default ReelItemCard
+
+const styles = StyleSheet.create({
+  img: {
+    width: screenWidth * 0.35,
+    height: screenHeight * 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: screenWidth * 0.35,
+    height: screenHeight * 0.25,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderRadius: 10,
+    borderWidth: 0.8,
+    borderColor: Colors.border,
+    overflow: 'hidden',
+  },
+  cardContent: {},
+  skeltonLoader: {
+    width: '100%',
+    height: '100%',
+  },
+})
