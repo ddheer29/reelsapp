ec932d5ffaed534f0603d225f710b7d29eb94d32
ddheer29 <divyangdheer11@gmail.com>
2025-02-19 23:17:06 +0530

    global feed

--- src/components/feed/GlobeFeed.tsx
+++ src/components/feed/GlobeFeed.tsx
@@ -1,9 +1,10 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { Animated, FlatList, ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import GlobeBg from '../../assets/images/globebg.jpg';
 import { screenHeight, screenWidth } from '../../utils/Scaling';
 import { useAppDispatch } from '../../redux/reduxHook';
 import { fetchFeedReel } from '../../redux/actions/reelAction';
+import ReelItemCard from './ReelItemCard';
 
 const GlobeFeed = () => {
   const dispatch = useAppDispatch();
@@ -17,15 +18,56 @@ const GlobeFeed = () => {
     setLoading(false);
   }
 
+  const renderItem = ({ item, index }: { item: any, index: number }) => {
+    const verticalShift = index % 2 === 0 ? -20 : 20;
+    return (
+      <Animated.View style={{ transform: [{ translateY: verticalShift }] }}>
+        <ReelItemCard
+          item={item}
+          loading={loading}
+          onPressReel={async () => { }}
+        />
+      </Animated.View>
+    )
+  }
+
   useEffect(() => {
     fetchFeed();
   }, [])
 
   return (
-    <ImageBackground source={GlobeBg} style={{ flex: 1, zIndex: -1 }}>
-      <View style={styles.gridContainer}>
-
-      </View>
+    <ImageBackground
+      source={GlobeBg}
+      style={{ flex: 1, zIndex: -1 }}
+      imageStyle={{ resizeMode: 'cover' }}
+    >
+      <Animated.View style={styles.container}>
+        <View style={styles.gridContainer}>
+          {
+            loading ? (
+              <FlatList
+                data={Array.from({ length: 16 })}
+                keyExtractor={(item, index) => index.toString()}
+                numColumns={4}
+                pinchGestureEnabled
+                renderItem={renderItem}
+                scrollEnabled={false}
+                contentContainerStyle={styles.flatlistContainer}
+              />
+            ) : (
+              <FlatList
+                data={data}
+                keyExtractor={(item, index) => index.toString()}
+                numColumns={4}
+                pinchGestureEnabled
+                renderItem={renderItem}
+                scrollEnabled={false}
+                contentContainerStyle={styles.flatlistContainer}
+              />
+            )
+          }
+        </View>
+      </Animated.View>
     </ImageBackground>
   )
 }
@@ -33,7 +75,7 @@ const GlobeFeed = () => {
 export default GlobeFeed
 
 const styles = StyleSheet.create({
-  constainer: {
+  container: {
     flex: 1,
     alignItems: 'center',
   },
@@ -42,5 +84,11 @@ const styles = StyleSheet.create({
     height: screenHeight * 2.9,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  flatlistContainer: {
+    paddingVertical: 20,
+    alignSelf: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   }
 })
