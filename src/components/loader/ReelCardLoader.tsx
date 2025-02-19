28e8e6ea09ad7cae3109d48f02db36fe06b7d5cd
ddheer29 <divyangdheer11@gmail.com>
2025-02-19 23:16:38 +0530

    real card skeletion loader

---
+++ src/components/loader/ReelCardLoader.tsx
@@ -0,0 +1,59 @@
+import { Animated, Easing, StyleSheet, Text, View, ViewStyle } from 'react-native'
+import React, { FC, useEffect, useRef } from 'react'
+import LinearGradient from 'react-native-linear-gradient';
+import { screenWidth } from '../../utils/Scaling';
+
+interface ReelCardLoaderProps {
+  style?: ViewStyle;
+}
+
+const ReelCardLoader: FC<ReelCardLoaderProps> = ({ style }) => {
+
+  const animatedValue = useRef(new Animated.Value(0)).current
+
+  const translateX = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-screenWidth * 0.35, screenWidth * 0.35]
+  })
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start()
+  }, [])
+
+  return (
+    <View style={[styles.ReelCardContainer, style]}>
+      <Animated.View style={[styles.skeletonLoader, { transform: [{ translateX }] }]}>
+        <LinearGradient
+          colors={['#333', '#444', '#555', '#444', '#333']}
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+          style={styles.gradient}
+        />
+      </Animated.View>
+    </View>
+  )
+}
+
+export default ReelCardLoader
+
+const styles = StyleSheet.create({
+  ReelCardContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#222'
+  },
+  skeletonLoader: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  gradient: {
+    width: '100%',
+    height: '100%',
+  }
+})
