c20c13656ceb0f614d22ab4639d0083158a0791a
ddheer29 <divyangdheer11@gmail.com>
2025-02-22 00:35:49 +0530

    iss

---
+++ src/components/reel/VideoItem.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VideoItem = () => {
+  return (
+    <View>
+      <Text>VideoItem</Text>
+    </View>
+  )
+}
+
+export default VideoItem
+
+const styles = StyleSheet.create({})
--- src/screens/reel/FeedReelScrollScreen.tsx
+++ src/screens/reel/FeedReelScrollScreen.tsx
@@ -1,9 +1,18 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React, { FC, useCallback, useEffect, useRef, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, Platform, StyleSheet, Text, TouchableOpacity, View, ViewToken } from 'react-native'
+import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { useDispatch } from 'react-redux'
 import CustomView from '../../components/global/CustomView'
 import { debounce } from 'lodash'
+import { screenHeight } from '../../utils/Scaling'
+import { fetchFeedReel } from '../../redux/actions/reelAction'
+import { useAppDispatch } from '../../redux/reduxHook'
+import { Colors } from '../../constants/Colors'
+import Loader from '../../assets/images/loader.jpg'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { goBack } from '../../utils/NavigationUtil'
+import { RFValue } from 'react-native-responsive-fontsize'
+import VideoItem from '../../components/reel/VideoItem'
 
 interface RouteProp {
   data: any[]
@@ -11,7 +20,7 @@ interface RouteProp {
 
 const FeedReelScrollScreen: FC = () => {
   const route = useRoute();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const routeParams = route?.params as RouteProp;
   const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState(0);
@@ -24,21 +33,70 @@ const FeedReelScrollScreen: FC = () => {
     itemVisiblePercentThreshold: 80,
   }).current;
 
+  const onViewableItemsChanged = useRef(
+    debounce(({ viewableItem }: { viewableItem: Array<ViewToken> }) => {
+      if (viewableItem.length > 0) {
+        setCurrentVisibleIndex(viewableItem[0].index || 0);
+      }
+    }, 100)
+  ).current;
 
-  const fetchFeedReel = useCallback(async (offset: number) => {
-    debounce()
-  }, [loading, hasMore, data, dispatch]);
+  const getItemLayout = useCallback((data: any, index: number) => ({
+    length: screenHeight,
+    offset: screenHeight * index,
+    index
+  }), []);
+
+  const removeDuplicate = (data: any) => {
+    const uniqueData = new Map();
+    data?.forEach((item: any) => {
+      if (!uniqueData.has(item._id)) {
+        uniqueData.set(item._id, item);
+      }
+    }
+    );
+    return Array.from(uniqueData.values());
+  }
+
+  const fetchFeed = useCallback(
+    debounce(async (offset: number) => {
+      if (loading || !hasMore) return;
+      setLoading(true);
+      try {
+        const newDara = await dispatch(fetchFeedReel(offset, 8));
+        setOffset(offset + 8);
+        if (newDara?.length < 8) {
+          setHasMore(false);
+        }
+        setData(removeDuplicate([...data, ...newDara]));
+      } catch (error) {
+        console.log('error', error);
+      } finally {
+        setLoading(false);
+      }
+    }, 200),
+    [loading, hasMore, data, dispatch]);
 
   const renderVideoList = useCallback(({ item, index }: { item: any, index: number }) => {
     return (
-      <View style={{ backgroundColor: 'red', flex: 1 }}>
-        <Text>hi</Text>
+      <View style={{ backgroundColor: 'red', flex: 1, height: screenHeight }}>
+        <Image
+          source={{ uri: item?.thumbUri }}
+          style={{
+            width: '100%',
+            height: screenHeight,
+            aspectRatio: 9 / 16,
+            resizeMode: 'cover'
+          }}
+        />
+        <VideoItem />
       </View>
     )
-  }, []);
+  }, [currentVisibleIndex]);
 
   const keyExtractor = useCallback((item: any) => item._id.toString(), []);
 
+  const memoizedValue = useMemo(() => renderVideoList, [currentVisibleIndex, data]);
 
   useEffect(() => {
     if (routeParams?.data) {
@@ -52,27 +110,71 @@ const FeedReelScrollScreen: FC = () => {
       <FlatList
         data={data || []}
         keyExtractor={keyExtractor}
-        renderItem={renderVideoList}
-        pagingEnabled
+        renderItem={memoizedValue}
         windowSize={2}
+        onEndReached={async () => {
+          await fetchFeed(offset);
+        }}
+        pagingEnabled
+        viewabilityConfig={viewabilityConfig}
         disableIntervalMomentum={true}
         removeClippedSubviews={true}
+        maxToRenderPerBatch={2}
+        getItemLayout={getItemLayout}
+        onViewableItemsChanged={onViewableItemsChanged}
         initialNumToRender={1}
         onEndReachedThreshold={0.1}
+        ListFooterComponent={() => {
+          {
+            loading ? <View style={styles.footer}>
+              <ActivityIndicator size='small' color={Colors.white} />
+            </View> : null
+          }
+        }}
         decelerationRate={'normal'}
         showsVerticalScrollIndicator={false}
-        viewabilityConfig={viewabilityConfig}
         scrollEventThrottle={16}
-        onEndReached={async () => {
-          await fetchFeedReel(offset);
-        }}
       />
+      <Image
+        source={Loader}
+        style={styles.thumbnail}
+      />
+      <View style={styles.backButton}>
+        <TouchableOpacity onPress={() => goBack()}>
+          <Icon name='arrow-back' size={RFValue(20)} color={Colors.white} />
+        </TouchableOpacity>
+      </View>
     </CustomView>
   )
 }
 
 export default FeedReelScrollScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backButton: {
+    position: 'absolute',
+    top: Platform.OS === 'ios' ? 60 : 20,
+    left: 10,
+    zIndex: 99,
+  },
+  footer: {
+    height: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  thumbnail: {
+    position: 'absolute',
+    zIndex: -2,
+    aspectRatio: 9 / 16,
+    width: '100%',
+    height: screenHeight,
+    alignSelf: 'center',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    resizeMode: 'stretch',
+  }
+})
 // ut920vDHOiJkMzHI
 // divyangdheer11
