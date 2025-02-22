0b925c0b18035842b222ea54be792e59d224de68
ddheer29 <divyangdheer11@gmail.com>
2025-02-22 15:34:14 +0530

    gf

--- src/redux/API.tsx
+++ src/redux/API.tsx
@@ -1,15 +1,11 @@
 import { Platform } from 'react-native';
 
 
-export const BASE_URL =
-  Platform.OS === 'android'
-    ? 'http://localhost:3000'
-    : 'http://localhost:3000';
-
+export const BASE_URL = "https://dheer-reels-app.onrender.com"
 
 export const CHECK_USERNAME = `${BASE_URL}/oauth/check-username`;
 export const REGISTER = `${BASE_URL}/oauth/register`;
 export const LOGIN = `${BASE_URL}/oauth/login`;
 export const REFRESH_TOKEN = `${BASE_URL}/oauth/refresh_token`;
 export const UPLOAD = `${BASE_URL}/file/upload`;
-export const GIPHY_API_KEY = 'YOUR_GIPHY_API_KEY';
+export const GIPHY_API_KEY = 'fxoON5WfxYwmtmpMSfeR6ft2s3ntHyNW';
