80479f32176d515653409903d567a4ae03760450
ddheer29 <divyangdheer11@gmail.com>
2025-03-15 00:16:01 +0530

    comment action

--- src/redux/actions/commentAction.tsx
+++ src/redux/actions/commentAction.tsx
@@ -1,4 +1,5 @@
 import { appAxios } from "../apiConfig";
+import { addComment } from "../reducers/commentSlice";
 
 export const getComments = (reelId: string, offset: number) => async (dispatch: any) => {
   try {
@@ -7,5 +8,15 @@ export const getComments = (reelId: string, offset: number) => async (dispatch: 
   } catch (error) {
     console.log("🚀 ~ getComments ~ error:", error);
     return [];
+  }
+};
+
+export const postComment = (data: any, commentsCount: number) => async (dispatch: any) => {
+  try {
+    const res = await appAxios.post(`/comment`, data);
+    dispatch(addComment({ reelId: data?.reelId, commentsCount: commentsCount + 1 }));
+    return res.data;
+  } catch (error) {
+    console.log("🚀 ~ postComment ~ error:", error)
   }
 };
--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userSlice from './reducers/userSlice';
 import followingSlice from './reducers/followingSlice';
 import likesSlice from './reducers/likeSlice';
+import CommentSlice from './reducers/commentSlice';
 
 const rootReducer = combineReducers({
   user: userSlice,
   following: followingSlice,
-  like: likesSlice
+  like: likesSlice,
+  comment: CommentSlice
 });
 
 export default rootReducer;
