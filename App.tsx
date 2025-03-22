0ce003512ce91342e917582d71a9730910c9909a
ddheer29 <divyangdheer11@gmail.com>
2025-03-22 13:59:31 +0530

    google auth fixed

--- App.tsx
+++ App.tsx
@@ -9,21 +9,14 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './src/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-// GoogleSignin.configure({
-//   webClientId:
-//     '118252792281-s50l7e855pa3oheb04kefeqeeeq4bnrc.apps.googleusercontent.com',
-//   forceCodeForRefreshToken: true,
-//   offlineAccess: false,
-//   iosClientId:
-//     '118252792281-7pomlrv6fu71bsfrcjlrdh1uvlkb66g0.apps.googleusercontent.com',
-// });
 
 GoogleSignin.configure({
-  scopes: ['email'], // Request email and profile info
-  webClientId: '118252792281-7pomlrv6fu71bsfrcjlrdh1uvlkb66g0.apps.googleusercontent.com', // Web Client ID (for Firebase or backend authentication)
+  webClientId:
+    '118252792281-s50l7e855pa3oheb04kefeqeeeq4bnrc.apps.googleusercontent.com',
   forceCodeForRefreshToken: true,
-  offlineAccess: true, // Allows you to refresh the token
-  iosClientId: '118252792281-s50l7e855pa3oheb04kefeqeeeq4bnrc.apps.googleusercontent.com', // iOS-specific client ID
+  offlineAccess: false,
+  iosClientId:
+    '118252792281-7pomlrv6fu71bsfrcjlrdh1uvlkb66g0.apps.googleusercontent.com',
 });
 
 const App = () => {
